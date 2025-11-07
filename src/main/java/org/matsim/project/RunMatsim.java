48a133986165cfec5de302b15666c3998bdafabc
lowIQ <sven.habermann@campus.tu-berlin.de>
2025-11-07 10:29:39 +0100

    Test commit with isolated Router gRPC proto, pom.xml and config.xml changes.

--- src/main/java/org/matsim/project/RunMatsim.java
+++ src/main/java/org/matsim/project/RunMatsim.java
@@ -18,12 +18,22 @@
  * *********************************************************************** */
 package org.matsim.project;
 
+import com.google.inject.name.Names;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.QSimConfigGroup;
+import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.events.SingleHandlerEventsManager;
+import org.matsim.core.router.RoutingModule;
 import org.matsim.core.scenario.ScenarioUtils;
+
+import java.beans.EventHandler;
+import java.io.IOException;
 
 /**
  * @author nagel
@@ -31,18 +41,25 @@ import org.matsim.core.scenario.ScenarioUtils;
  */
 public class RunMatsim{
 
+    public static final String outputDirectory = "output/gRPCEventTest" ;
+
 	public static void main(String[] args) {
 
 		Config config;
 		if ( args==null || args.length==0 || args[0]==null ){
-			config = ConfigUtils.loadConfig( "scenarios/equil/config.xml" );
+			config = ConfigUtils.loadConfig( "scenarios/equil/config1.xml" );
 		} else {
 			config = ConfigUtils.loadConfig( args );
 		}
 
+        config.controller().setOutputDirectory(outputDirectory);
 		config.controller().setOverwriteFileSetting( OverwriteFileSetting.deleteDirectoryIfExists );
+        //config.qsim().setVehiclesSource(QSimConfigGroup.VehiclesSource.fromVehiclesData);
+//        config.global().setNumberOfThreads(1);
+//        config.qsim().setNumberOfThreads(1);
 
-		// possibly modify config here
+        // possibly modify config here
+		 config.controller().setLastIteration(2);
 
 		// ---
 		
@@ -53,16 +70,38 @@ public class RunMatsim{
 		// ---
 		
 		Controler controler = new Controler( scenario ) ;
-		
+
 		// possibly modify controler here
+
+        IsolatedRouterClient grpcRouterClient = new IsolatedRouterClient("localhost", 50051);
+        controler.addOverridingModule(new AbstractModule() {
+            @Override
+            public void install() {
+                addRoutingModuleBinding("car").to(GrpcRoutingModule.class);
+            }
+        });
+
+        // add the events handlers
+        controler.addOverridingModule(new AbstractModule(){
+            @Override public void install() {
+                bind(IsolatedRouterClient.class).toInstance(grpcRouterClient);
+				this.addEventHandlerBinding().toInstance( new GRPCEventHandler() );
+            }
+        });
 
 //		controler.addOverridingModule( new OTFVisLiveModule() ) ;
 
 //		controler.addOverridingModule( new SimWrapperModule() );
 		
 		// ---
-		
+//		System.out.println("Der Controller läuft an...");
 		controler.run();
-	}
+//        System.out.println("Main ist zu Ende...");
+        try {
+            grpcRouterClient.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 	
 }
